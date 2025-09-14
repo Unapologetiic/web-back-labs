5e6a00fb41bcb56e437284de26721413542cdfbd
Unapologetiic <kudearovaana@gmail.com>
2025-09-14 19:41:51 +0700

    исправление адресов

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask (__name__)
 def not_found(err):
     return "нет такой страницы", 404
 
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
@@ -18,7 +18,7 @@ def web():
             'Content-Type': 'text/plain; charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Кудеярова Яна Олеговна"
     group = "ФБИ-31"
@@ -34,7 +34,7 @@ def author():
                 </body>
             </html>"""
 
-@app.route("/image")
+@app.route("/lab1/image")
 def image():
     path = url_for("static", filename="oak.jpg")
     css_path = url_for("static", filename="lab1.css")
@@ -52,7 +52,7 @@ def image():
 '''
 count = 0
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count += 1
@@ -73,7 +73,7 @@ def counter():
     </body>
 </html>
 '''
-@app.route('/reset_counter')
+@app.route('/lab1/reset_counter')
 def reset_counter():
     global count
     count = 0
@@ -87,7 +87,7 @@ def reset_counter():
 </html>
 '''
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
     return redirect("/author")
 
