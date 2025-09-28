8b9f80782c97aa84ad3061d89ff2466783480c22
Unapologetiic <kudearovaana@gmail.com>
2025-09-28 21:35:03 +0700

    Имя студента

--- app.py
+++ app.py
@@ -339,4 +339,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Яна Кудеярова'
+    return render_template('example.html', name=name)
