c8d0fbadb301e98ad292742b3d3d01231f6fbb36
Unapologetiic <kudearovaana@gmail.com>
2025-09-28 21:38:26 +0700

    передача переменных в шаблон

--- app.py
+++ app.py
@@ -340,4 +340,5 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Яна Кудеярова'
-    return render_template('example.html', name=name)
+    number = '2'
+    return render_template('example.html', name=name, number=number)
