c99b8393b98d62c7b429b2d8fb2add8e1bce81a7
Unapologetiic <kudearovaana@gmail.com>
2025-09-29 21:29:47 +0700

    Цикл

--- app.py
+++ app.py
@@ -342,5 +342,13 @@ def example():
     name = 'Яна Кудеярова'
     group = 'ФБИ-31'
     course = '3 курс'
-    lab_num = '2'
-    return render_template('example.html', name=name, group=group, course=course, lab_num=lab_num)
+    lab_num = 2
+    fruits = [
+        {'name': 'яблоки', 'price':100},
+        {'name': 'груши', 'price':120},
+        {'name': 'апельсины', 'price':80},
+        {'name': 'мандарины', 'price':90},
+        {'name': 'манго', 'price':321}
+    ]
+    return render_template('example.html', name=name, group=group, course=course, 
+                           lab_num=lab_num, fruits=fruits)
