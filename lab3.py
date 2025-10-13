08a9f91d2fddea035ebc2148a5ad926170376c9f
Unapologetiic <kudearovaana@gmail.com>
2025-10-13 21:08:42 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -17,6 +17,7 @@ def cookie():
     resp.set_cookie('name_color', 'magenta')
     return resp
 
+
 @lab3.route('/lab3/del_cookie')
 def del_cookie():
     resp = make_response(redirect('/lab3/'))
@@ -24,4 +25,12 @@ def del_cookie():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
 
