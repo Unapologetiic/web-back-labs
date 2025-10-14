699463a6ddfcf7d7ef4e032db05fc3cc6b365165
Unapologetiic <kudearovaana@gmail.com>
2025-10-15 00:21:35 +0700

    Очистка всех кук

--- lab3.py
+++ lab3.py
@@ -152,3 +152,13 @@ def ticket2():
                          fio=fio, shelf=shelf, linen=linen, luggage=luggage,
                          age=age, departure=departure, destination=destination,
                          date=date, insurance=insurance, price=price)
+
+
+@lab3.route('/lab3/clear_settings')
+def clear_settings():
+    resp = make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('font_family')
+    return resp
