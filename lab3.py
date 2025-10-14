fc3185a66b7fe9e630b56bed1e8d18c942570bbf
Unapologetiic <kudearovaana@gmail.com>
2025-10-14 10:44:07 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -64,7 +64,34 @@ def pay():
     
     return render_template('lab3/pay.html', price=price)
 
+
 @lab3.route('/lab3/success')
 def success():
     price = request.args.get('price', 0)
     return render_template('lab3/success.html', price=price)
+
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')   
+    bg_color = request.args.get('bg_color')
+    font_size = request.args.get('font_size')
+    
+    if color or bg_color or font_size:
+        resp = make_response(redirect('/lab3/settings'))
+        if color:
+            resp.set_cookie('color', color)
+        if bg_color:
+            resp.set_cookie('bg_color', bg_color)
+        if font_size:
+            resp.set_cookie('font_size', font_size)
+        return resp
+    
+    color_from_cookie = request.cookies.get('color', '#000000')
+    bg_color_from_cookie = request.cookies.get('bg_color', '#ffffff')
+    font_size_from_cookie = request.cookies.get('font_size', '16px')
+    
+    return render_template('lab3/settings.html', 
+                         color=color_from_cookie,
+                         bg_color=bg_color_from_cookie,
+                         font_size=font_size_from_cookie)
