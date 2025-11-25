1d73795f35de6655d8e5bcdf0cc4dca2d935d1e3
Unapologetiic <kudearovaana@gmail.com>
2025-11-25 16:01:13 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -52,6 +52,47 @@ def api():
                 'result': 'success',
                 'id': id
             }
+
+           
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':  # Офис не арендован
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office is not booked'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:  # Офис арендован другим пользователем
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'Office is booked by another user'
+                        },
+                        'id': id
+                    }
+                # Снимаем аренду
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+        # Если офис не найден
+        return {
+            'jsonrpc': '2.0',
+            'error': {
+                'code': 5,
+                'message': 'Office not found'
+            },
+            'id': id
+        }
+
     return {
         'jsonrpc': '2.0',
         'error': {
