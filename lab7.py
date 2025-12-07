dcf1ec22db4d87181cc8fbfb666928fe4adc1166
Unapologetiic <kudearovaana@gmail.com>
2025-12-07 19:25:43 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -6,3 +6,45 @@ lab7 = Blueprint('lab7', __name__)
 @lab7.route('/lab7/')
 def lab():
     return render_template('lab7/index.html')
+
+films = [
+    {
+        "title": "The Gentlemen",
+        "title_ru": "Джентльмены",
+        "year": 2019,
+        "description": "Гангстеры всех мастей делят наркоферму. Закрученная экшен-комедия Гая Ричи с Мэттью Макконахи и Хью Грантом"
+    },
+    {
+        "title": "Shutter Island",
+        "title_ru": "Остров проклятых",
+        "year": 2009,
+        "description": "Двое приставов расследуют побег пациентки из мрачной психбольницы. Параноидальный триллер с Леонардо ДиКаприо"
+    },
+    {
+        "title": "Fight Club",
+        "title_ru": "Бойцовский клуб",
+        "year": 1999,
+        "description": "Бессонница, драки и мыло. Контркультурный шедевр Дэвида Финчера, который можно пересматривать бесконечно"
+    },
+    {
+        "title": "1+1",
+        "title_ru": "1+1",
+        "year": 2011,
+        "description": "Аристократ на коляске нанимает в сиделки бывшего заключенного. Искрометная французская комедия с Омаром Си"
+    },
+    {
+        "title": "The Green Mile",
+        "title_ru": "Зеленая миля",
+        "year": 1999,
+        "description": "Пол Эджкомб не верил в чудеса. Пока не столкнулся с одним из них"
+    },
+]
+
+
+@lab7.route('/lab7/rest-api/films/', methods=['GET'])
+def get_films():
+    return films
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
+def get_film(id):
+    return films[id]
